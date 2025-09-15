350bb8b66949393a9f7608d0ea3e74730cb72a84
RoqueGames <roquegames77@gmail.com>
2025-09-15 11:36:57 -0300

    server.js

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
-const WebSocket = require("ws");
+extends Node
 
-const port = process.env.PORT || 8080;
-const wss = new WebSocket.Server({ port });
+const PORT = 8080
+const MESSAGE_TYPE = "message"
+var server = TCPServer.new()
+var clients = []
 
-wss.on("connection", (ws) => {
-  console.log("Novo cliente conectado");
+func _ready():
+    if server.listen(PORT) == OK:
+        print("Servidor iniciado na porta ", PORT)
+    else:
+        print("Erro ao iniciar servidor")
 
-  ws.on("message", (msg) => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        // A CORREÇÃO ESTÁ AQUI: envie 'msg' diretamente
-        client.send(msg);
-      }
-    });
-  });
+func _process(_delta):
+    # Aceitar novas conexões
+    if server.is_connection_available():
+        var client = server.take_connection()
+        clients.append(client)
+        print("Novo cliente conectado: ", client)
+    
+    # Processar mensagens dos clientes
+    for client in clients:
+        if client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+            if client.get_available_bytes() > 0:
+                var message = client.get_string(client.get_available_bytes())
+                broadcast(message, client)
+        else:
+            clients.erase(client)
+            print("Cliente desconectado")
 
-  ws.on("close", () => console.log("Cliente desconectado"));
-});
-
-console.log("Servidor relay rodando na porta", port);
-
+func broadcast(message, sender):
+    var json = JSON.new()
+    var error = json.parse(message)
+    
+    if error == OK:
+        var data = json.data
+        if data is Dictionary and data.has("type") and data["type"] == MESSAGE_TYPE:
+            # Enviar para todos os clientes exceto o remetente
+            for client in clients:
+                if client != sender and client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+                    client.put_string(message + "\n")
+    else:
+        print("JSON inválido: ", message)
